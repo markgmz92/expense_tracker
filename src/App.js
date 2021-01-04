572c3de937bd838cbb2e4b51ac26e523916e4e29
markgmz92 <markgmz92@gmail.com>
2021-01-04 16:58:39 +0800

    [ET - 003] Added global css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
